46d2f1bb2e6b37eec790743ba9fda35ac2ebb762
byteful <info@byteful.me>
2023-07-28 00:25:35 -0500

    Cover more events for hub spawn

--- src/main/java/mcheroes/core/hub/HubFeature.java
+++ src/main/java/mcheroes/core/hub/HubFeature.java
@@ -6,10 +6,13 @@ import mcheroes.core.locale.LocaleAdapter;
 import mcheroes.core.locale.Messages;
 import mcheroes.core.utils.Permissions;
 import mcheroes.core.utils.Position;
+import mcheroes.core.utils.Scheduler;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.spigotmc.event.player.PlayerSpawnLocationEvent;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.bukkit.BukkitCommandHandler;
@@ -18,18 +21,23 @@ public class HubFeature implements CoreFeature, Listener {
     private final BukkitCommandHandler commandHandler;
     private final DataStore dataStore;
     private final LocaleAdapter locale;
+    private final Scheduler scheduler;
     private Location cachedLocation;
 
-    public HubFeature(BukkitCommandHandler commandHandler, DataStore dataStore, LocaleAdapter locale) {
+    public HubFeature(BukkitCommandHandler commandHandler, DataStore dataStore, LocaleAdapter locale, Scheduler scheduler) {
         this.commandHandler = commandHandler;
         this.dataStore = dataStore;
         this.locale = locale;
+        this.scheduler = scheduler;
     }
 
     @Override
     public void load() {
         commandHandler.register(this);
         cachedLocation = dataStore.getHubSpawn().toBukkit();
+
+        cachedLocation.getChunk().setForceLoaded(true);
+        cachedLocation.getChunk().load();
     }
 
     @Override
@@ -40,6 +48,17 @@ public class HubFeature implements CoreFeature, Listener {
     @EventHandler
     public void on(PlayerSpawnLocationEvent event) {
         event.setSpawnLocation(cachedLocation);
+    }
+
+    @EventHandler
+    public void on(PlayerJoinEvent event) {
+        scheduler.later(() -> event.getPlayer().teleport(cachedLocation), 1L);
+    }
+
+    @EventHandler
+    public void on(PlayerRespawnEvent event) {
+        // TODO: disable this event if current minigame overrides respawn mechanics
+        event.setRespawnLocation(cachedLocation);
     }
 
     @Command("hub")
@@ -54,7 +73,13 @@ public class HubFeature implements CoreFeature, Listener {
             return;
         }
 
+        if (cachedLocation != null) {
+            cachedLocation.getChunk().setForceLoaded(false);
+        }
+
         cachedLocation = sender.getLocation();
+        cachedLocation.getChunk().setForceLoaded(true);
+
         dataStore.setHubSpawn(Position.of(cachedLocation));
         sender.sendMessage(Messages.HUB_SET_SUCCESSFULLY.build(locale));
     }
